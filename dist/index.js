6d7e1a602b002c61d004f648a4b4772308099da4
Adrianne Shean Pacifico <adriannepersonal05@gmail.com>
2026-09-08 10:59:09 -0600

    Initial commit

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addition(a, b) {
+    return a + b;
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,23 @@
+function addition(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(addition(3, 4));
+
+function subtraction(a: number, b: number): number {
+  return a - b;
+}
+
+console.log(subtraction(4, 3));
+
+function multiplication(a: number, b: number): number {
+  return a ** b;
+}
+
+console.log(multiplication(3, 4));
+
+function division(a: number, b: number): number {
+  return a / b;
+}
+
+console.log(division(3, 4));
